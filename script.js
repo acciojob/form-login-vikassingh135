6adfc32b0a11140b1ddc3cce1eec9c6900fa48ca
Vikas Singh <69408149+vikassingh135@users.noreply.github.com>
2023-05-23 00:25:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+	let firstName = document.getElementById("fname").value;
+	let lastName = document.getElementById("lname").value;
+	alert(firstName,lastName);
 
 }
